4e5756b436afa14eb094d1f6eb4f5b0475815c00
martingaidoo <martingaido00@gmail.com>
2024-12-26 16:25:30 -0300

    se soluciona el probema de las horas

--- src/components/ComponentDownCount.tsx
+++ src/components/ComponentDownCount.tsx
@@ -5,7 +5,7 @@ export const ComponentDownCount = () => {
     console.log("v2")
     const now = new Date();
     const may5th = new Date(now.getFullYear()+1, 0, 1); // Los meses en JavaScript empiezan en 0, por lo que mayo es 4
-    const millisecondsUntilMay4th = may5th.getTime() - now.getTime() + 7200000;
+    const millisecondsUntilMay4th = may5th.getTime() - now.getTime();
 
     const countToDate = new Date().setHours(new Date().getHours()) + millisecondsUntilMay4th
 
