5cccbeafd52a5d37ba7e7e422676f2a5d66df04a
martingaidoo <martingaido00@gmail.com>
2024-12-26 15:44:20 -0300

    se buildea

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/"
-})
+  base: '/', // Ruta base, ajusta esto si es necesario dependiendo de tu despliegue
+  build: {
+    outDir: 'build', // Directorio de salida
+    rollupOptions: {
+      input: './index.html', // Ruta de entrada de tu archivo HTML
+    },
+  },
+});
